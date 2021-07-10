6887844364d4a0b73f67c9129cfd4adcd58a081d
raphael <raphael.fernandes@intelie.com.br>
2021-07-10 19:51:43 -0300

    Product details

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Container} from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <main>
+      <Container>
+        <HomeScreen/>
+      </Container>
+      
+      </main>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
